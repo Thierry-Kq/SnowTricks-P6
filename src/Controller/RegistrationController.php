9194da6a464f8b519e10414d61a35bcb59631a33
K-pc-Ubuntu <kasskq@gmail.com>
2020-11-09 19:47:27 +0100

    Email / Activation Token
    When user register, send mail with activation token

--- src/Controller/RegistrationController.php
+++ src/Controller/RegistrationController.php
@@ -4,6 +4,8 @@ namespace App\Controller;
 
 use App\Entity\User;
 use App\Form\RegistrationFormType;
+use App\Repository\UserRepository;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Mailer\MailerInterface;
@@ -11,6 +13,7 @@ use Symfony\Component\Mime\Email;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\Security\Core\Encoder\UserPasswordEncoderInterface;
+use Symfony\Component\Security\Core\Exception\CustomUserMessageAuthenticationException;
 
 class RegistrationController extends AbstractController
 {
@@ -34,24 +37,20 @@ class RegistrationController extends AbstractController
                     $form->get('plainPassword')->getData()
                 )
             );
-
-            // TODO : email is working, email for account creation?
-            // ATTENTION ! ERROR SI LE MAIL 'does not comply with addr-spec of RFC 2822.' par exemple eazezae (pas de @.com)
-//            $email = (new Email())
-//                ->from('aze@gmail.com')
-//                ->to($user->getEmail())
-//                ->subject('Salut Toa')
-//                ->text('Texte html?')
-//                ->html('<p>Html ! <br> Tavu ?</p>');
-////
-//            $mailer->send($email);
-
-
+            $user->setActivationToken(md5(uniqid()));
             $entityManager = $this->getDoctrine()->getManager();
             $entityManager->persist($user);
             $entityManager->flush();
 
-            // do anything else you need here, like send an email
+            // TODO ATTENTION ! ERROR SI LE MAIL 'does not comply with addr-spec of RFC 2822.' par exemple eazezae (pas de @.com)
+            $email = (new Email())
+                ->from('noreply@snowtricks.com')
+                ->to($user->getEmail())
+                ->subject('Snowtricks - Validation de votre inscription')
+//                ->text('Texte html?')
+                ->html($this->renderView('emails/activation.html.twig', ['token' => $user->getActivationToken()]));
+
+            $mailer->send($email);
 
             return $this->redirectToRoute('homepage');
         }
@@ -62,5 +61,31 @@ class RegistrationController extends AbstractController
                 'registrationForm' => $form->createView(),
             ]
         );
+    }
+
+    /**
+     * @Route("/activation/{token}", name="activation")
+     */
+    public function activation(
+        UserRepository $userRepository,
+        EntityManagerInterface $entityManager,
+        $token
+    ) {
+        $user = $userRepository->findOneBy(['activationToken' => $token]);
+
+        if (!$user) {
+            $this->addFlash(
+                'notice',
+                'Not Found'
+            );
+//            throw new CustomUserMessageAuthenticationException('Username could not be found.');
+            // TODO : 404 ?
+        } else {
+            $user->setActivationToken(null);
+            $entityManager->persist($user);
+            $entityManager->flush();
+        }
+
+        return $this->redirectToRoute('homepage');
     }
 }
