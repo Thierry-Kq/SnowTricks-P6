f89cd73abf1688c0f5ca2498c096786ba4446426
K-pc-Ubuntu <kasskq@gmail.com>
2020-11-22 15:11:44 +0100

    Comments
    Entity Comments, fixtures update for comments

--- src/Entity/Tricks.php
+++ src/Entity/Tricks.php
@@ -52,10 +52,16 @@ class Tricks
      */
     private $video;
 
+    /**
+     * @ORM\OneToMany(targetEntity=Comment::class, mappedBy="trick", orphanRemoval=true)
+     */
+    private $comments;
+
     public function __construct()
     {
         $this->images = new ArrayCollection();
         $this->video = new ArrayCollection();
+        $this->comments = new ArrayCollection();
     }
 
     public function getId(): ?int
@@ -174,6 +180,37 @@ class Tricks
     {
         if ($this->video->contains($video)) {
             $this->video->removeElement($video);
+        }
+
+        return $this;
+    }
+
+    /**
+     * @return Collection|Comment[]
+     */
+    public function getComments(): Collection
+    {
+        return $this->comments;
+    }
+
+    public function addComment(Comment $comment): self
+    {
+        if (!$this->comments->contains($comment)) {
+            $this->comments[] = $comment;
+            $comment->setTrick($this);
+        }
+
+        return $this;
+    }
+
+    public function removeComment(Comment $comment): self
+    {
+        if ($this->comments->contains($comment)) {
+            $this->comments->removeElement($comment);
+            // set the owning side to null (unless already changed)
+            if ($comment->getTrick() === $this) {
+                $comment->setTrick(null);
+            }
         }
 
         return $this;
